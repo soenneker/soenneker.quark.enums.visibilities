6ef1217e4cb15b776f93578ce6eaceaa0d0ba431
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:30 -0500

    TUnit conversion

--- test/Soenneker.Quark.Enums.Visibilities.Tests/VisibilityTests.cs
+++ test/Soenneker.Quark.Enums.Visibilities.Tests/VisibilityTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Enums.Visibilities.Tests;
 
 public sealed class VisibilityTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
